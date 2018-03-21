210fa989a752496c9964f515c38ce5a0d0b6ba95
Ben Hurst <BHurst8@gmail.com>
2018-03-21 10:06:17 -0700

    cross-origin tainting error

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//'use strict';
+
+var finalImgEl1 = document.getElementById('test');
+console.log(finalImgEl1);
+var colorThief1 = new ColorThief();
+var c1 = colorThief1.getColor(finalImgEl1);
+console.log(c1);
